fcd17a55f7cb3e4bfeb9268a234fd28f3fb4ec35
Elor Itzkovitz <96355233+Elor-Itz@users.noreply.github.com>
2024-07-24 19:48:52 +0300

    Small fix

--- frontEnd/public/itemDisplayModule.js
+++ frontEnd/public/itemDisplayModule.js
@@ -18,7 +18,7 @@ export async function fetchItems() {
 export async function displayItems(items, container, callbackFunc) {
     container.innerHTML = "";
 
-    if (items.length === 0 || items == null) {             
+    if (items.length === 0 || items == null) {
         const noResults = document.createElement('div');
         noResults.textContent = 'No items found.';
         container.appendChild(noResults);
@@ -38,45 +38,60 @@ export async function displayItems(items, container, callbackFunc) {
             price.textContent = "Price: $" + item.price;
 
             const stock = document.createElement("p");
-            stock.textContent = item.stock > 0 ? "In stock" : "Out of stock";  
-                        
-            const descriptionPopup = document.createElement("div");
-            descriptionPopup.classList.add("description-popup");
-            descriptionPopup.innerHTML = `${item.description}<br><br><strong>Country:</strong> ${item.madeIn}<br><br><strong>Category:</strong> ${item.category}`;
-
-            const viewDetailsButton = document.createElement("button");
-            viewDetailsButton.classList.add("view-details");
-            
-            // Show the popup when the mouse hovers over the button
-            viewDetailsButton.addEventListener("mouseover", () => {
-                showPopup(descriptionPopup);
-            });
-
-            // Hide the popup when the mouse leaves the button
-            viewDetailsButton.addEventListener("mouseout", () => {
-                hidePopup(descriptionPopup);
-            })
-
-            // Show the popup when the mouse clicks on the button
-            viewDetailsButton.addEventListener("click", () => {
-                showPopup(descriptionPopup);
-            });
-
-            // Hide the popup when the mouse hovers over the image
-            img.addEventListener("mouseover", () => {
-                hidePopup(descriptionPopup);
-            });
+            stock.textContent = item.stock > 0 ? "In stock" : "Out of stock";
 
             const button = document.createElement("button");
             button.textContent = callbackFunc === addToCart ? "Add to Cart" : "Remove from Cart";
-            button.addEventListener("click", () => callbackFunc(item));            
+            button.addEventListener("click", () => callbackFunc(item));
 
-            itemDiv.appendChild(img);            
-            itemDiv.appendChild(name); 
-            itemDiv.appendChild(viewDetailsButton);
-            itemDiv.appendChild(descriptionPopup);          
-            itemDiv.appendChild(price);
-            itemDiv.appendChild(stock);
+            const shopContainer = document.getElementById('itemContainer');
+            const cartContainer = document.getElementById('cartItems');
+
+
+            // Check if appending to shop or cart display
+            if (container === cartContainer) {
+                itemDiv.appendChild(img);
+                itemDiv.appendChild(name);
+                itemDiv.appendChild(price);
+            }
+
+            else if (container === shopContainer) {
+                itemDiv.appendChild(img);
+                itemDiv.appendChild(name);
+                
+                const descriptionPopup = document.createElement("div");
+                descriptionPopup.classList.add("description-popup");
+                descriptionPopup.innerHTML = `${item.description}<br><br><strong>Country:</strong> ${item.madeIn}<br><br><strong>Category:</strong> ${item.category}`;
+
+                const viewDetailsButton = document.createElement("button");
+                viewDetailsButton.classList.add("view-details");
+
+                // Show the popup when the mouse hovers over the button
+                viewDetailsButton.addEventListener("mouseover", () => {
+                    showPopup(descriptionPopup);
+                });
+
+                // Hide the popup when the mouse leaves the button
+                viewDetailsButton.addEventListener("mouseout", () => {
+                    hidePopup(descriptionPopup);
+                })
+
+                // Show the popup when the mouse clicks on the button
+                viewDetailsButton.addEventListener("click", () => {
+                    showPopup(descriptionPopup);
+                });
+
+                // Hide the popup when the mouse hovers over the image
+                img.addEventListener("mouseover", () => {
+                    hidePopup(descriptionPopup);
+                });
+
+                itemDiv.appendChild(viewDetailsButton);
+                itemDiv.appendChild(descriptionPopup);
+                itemDiv.appendChild(price);
+                itemDiv.appendChild(stock);
+            }
+            
             itemDiv.appendChild(button);
 
             container.appendChild(itemDiv);
@@ -85,9 +100,9 @@ export async function displayItems(items, container, callbackFunc) {
 }
 
 // Function to show the popup
-function showPopup(popup) {    
+function showPopup(popup) {
     popup.style.display = 'block';
-    popup.style.position = 'absolute';    
+    popup.style.position = 'absolute';
 }
 
 // Function to hide the popup
